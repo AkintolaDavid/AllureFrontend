4c460236ca8a90142f5e42b30ee12d992e3009f5
AkintolaDavid <akintoladvaid66@gmail.com>
2024-10-21 07:36:50 +0100

    m

--- src/Items.jsx
+++ src/Items.jsx
@@ -16,29 +16,31 @@ const Items = ({ id, name, image, price, description }) => {
           className="w-[100%] h-40 object-cover rounded-md mb-2"
         />
       </Link>
-      <div className="w-full flex items-center  justify-between px-3 sm:px-5">
-        <h2 className="text-md font-semibold mb-1 h-[50px]">{name}</h2>
+      <div className="w-full flex-col px-3 sm:px-5">
+        <h2 className="text-md font-semibold mb-1 h-[70px]">{name}</h2>
 
-        <p className="text-gray-600 mb-2 flex items-center">
-          {" "}
-          <TbCurrencyNaira />
-          {price}
-        </p>
-        <button
-          onClick={() => toggleLike({ _id: id, name, image, price })} // Use _id here
-          className={`p-2 rounded-full transition duration-300 ${
-            liked ? "bg-red-500 text-white" : "bg-gray-300 text-gray-600"
-          }`}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            viewBox="0 0 20 20"
-            fill="currentColor"
+        <div className="flex items-center  justify-between">
+          <p className="text-gray-600 mb-2 flex items-center">
+            {" "}
+            <TbCurrencyNaira />
+            {price}
+          </p>
+          <button
+            onClick={() => toggleLike({ _id: id, name, image, price })} // Use _id here
+            className={`p-2 rounded-full transition duration-300 ${
+              liked ? "bg-red-500 text-white" : "bg-gray-300 text-gray-600"
+            }`}
           >
-            <path d="M3.172 5.172a4 4 0 010 5.656L10 16l6.828-5.172a4 4 0 10-5.656-5.656L10 6.343 8.828 5.172a4 4 0 00-5.656 0z" />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M3.172 5.172a4 4 0 010 5.656L10 16l6.828-5.172a4 4 0 10-5.656-5.656L10 6.343 8.828 5.172a4 4 0 00-5.656 0z" />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
